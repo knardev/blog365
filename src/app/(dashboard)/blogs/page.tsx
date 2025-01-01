54404b97257776cd60ccdb1f1f6fd128db5a28b9
ysung327 <ysung327@naver.com>
2025-01-01 18:29:35 +0900

    edit table columns

--- src/app/(dashboard)/blogs/page.tsx
+++ src/app/(dashboard)/blogs/page.tsx
@@ -19,14 +19,16 @@ export default async function Page() {
 
   // 현재 UTC 기준 시간을 한국시간으로 변환
   const KST = "Asia/Seoul";
-  const nowInKST = toZonedTime(new Date(), "Asia/Seoul"); // 수정된 호출
+  const now = new Date();
 
   // 한국시간 기준 어제 날짜 객체 생성
-  const yesterdayDateInKST = subDays(nowInKST, 1);
+  const yesterdayDate = subDays(now, 1);
+  console.log("nowInKST", yesterdayDate);
 
   // 지난 30일의 날짜를 생성
   const allDates: string[] = Array.from({ length: 30 }, (_, index) => {
-    const date = subDays(yesterdayDateInKST, index); // 어제 날짜부터 하나씩 줄어드는 날짜 계산
+    const date = subDays(yesterdayDate, index); // 어제 날짜부터 하나씩 줄어드는 날짜 계산
+    // console.log(formatInTimeZone(date, KST, "yyyy-MM-dd"));
     return formatInTimeZone(date, KST, "yyyy-MM-dd"); // 한국시간 기준으로 포맷팅
   });
 
